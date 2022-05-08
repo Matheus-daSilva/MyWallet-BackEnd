93570d334f36a455ca1f3f5fd25e315c27e74ae0
Matheus-daSilva <matheus2001silva@live.com>
2022-05-07 23:04:56 -0300

    add: app.post(/login)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import bcrypt from 'bcrypt';
 import joi from "joi";
 import dotenv from "dotenv";
+import { v4 as uuid } from 'uuid';
 import { MongoClient } from "mongodb";
 
 const app = express();
@@ -22,38 +23,71 @@ app.post("/register", async (req, res) => {
         passwordConfirmation: req.body.passwordConfirmation
     }
 
+    const getEmail = user.email;
+
     let userSchema = joi.object({
         name: joi.string().required(),
         email: joi.string().email().required(),
         password: joi.string().required(),
-        passwordConfirmation: joi.string().required()
+        passwordConfirmation: joi.string().required(),
     });
 
     const validation = userSchema.validate(user);
 
     if (validation.error) {
-        return res.sendStatus(422);
+        console.log(validation.error)
+        console.log(user)
+        return res.status(422).send("preencha todos os campos corretamente")
     }
 
     if (user.password === user.passwordConfirmation) {
         passwordHash = bcrypt.hashSync(user.password, 10);
     } else {
-       return res.status(422).send("incorrect passwords");
+        return res.status(422).send("preencha todos os campos corretamente");
     }
 
     delete user.passwordConfirmation;
-
     try {
         await mongoClient.connect();
         const db = mongoClient.db("users");
-        await db.collection("usersInfos").insertOne({ ...user, password: passwordHash });
-        res.sendStatus(201);
+        const contains = await db.collection("userInfos").findOne({ getEmail });
+        if (!contains) {
+            await db.collection("userInfos").insertOne({ ...user, password: passwordHash });
+            console.log(user)
+            res.sendStatus(201);
+        } else {
+            res.status(409).send("usuário já cadastrado")
+        }
         mongoClient.close();
 
     } catch (e) {
         res.sendStatus(500);
         mongoClient.close();
     }
+})
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+    const token = uuid();
+
+    try {
+        mongoClient.connect();
+        const db = mongoClient.db("users");
+        const user = await db.collection('userInfos').findOne({ email });
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = uuid.v4();
+            await db.collection("sessions").insertOne({
+                userId: user._id,
+                token
+            })
+            res.send(token);
+        } else {
+            return res.status(422).send("senha ou usuário incorretos");
+        }
+    } catch(e) {
+        return res.sendStatus(500)
+    }
+
 })
 
 app.listen(5000);
